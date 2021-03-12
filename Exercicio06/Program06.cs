3e4c01e3e946b70c4f06b015284b39dafb705486
John-Stefan <johnstn1545@hotmail.com>
2021-03-11 21:31:25 -0300

    Master - Finalização da prova

--- Exercicio06/Program06.cs
+++ Exercicio06/Program06.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercicio06
 {
@@ -6,7 +7,31 @@ namespace Exercicio06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] valoresCaracteres;
+            List<int> valoresInteiros = new List<int>();
+            int[] resultado = new int[2];
+
+            do
+            {
+                Console.Write("\n\nDigite os valores: ");
+                string valores = Console.ReadLine();
+
+                valoresCaracteres = valores.Split(" ");
+
+                foreach (string valor in valoresCaracteres)
+                {
+                    if (Convert.ToInt32(valor) >= 10 && Convert.ToInt32(valor) <= 20)
+                        resultado[0]++;
+
+                    else
+                        resultado[1]++;
+                }
+
+                Console.Write($"In: {resultado[0]}\nOut: {resultado[1]}\n");
+                Console.ReadLine();
+
+                Console.Write("Deseja continuar? S/N: ");                
+            } while (Console.ReadKey().Key == ConsoleKey.S);
         }
     }
 }
